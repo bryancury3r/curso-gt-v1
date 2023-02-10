ffbb3333e830fe6ce5d039f5764b06912f061e24
bryancury3r <gabriel_bryancury@outlook.com>
2023-02-09 21:15:15 -0300

    remove navAction

--- js/script.js
+++ js/script.js
@@ -14,3 +14,13 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu("nav-toggle", "nav");
+
+// ----------------- REMOVER MENU MOBILE ---------------//
+
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  const navMenu = document.getElementById("nav");
+
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
